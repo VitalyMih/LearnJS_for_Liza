fd15e2b147765e3f2a5150f2ee5cce97b57819ce
LizavettaZ <ehkizaveta13@gmail.com>
2022-04-11 14:45:23 +0300

    module 2 tasks

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,2 +1,11 @@
 function sumSalary(salaries) {
+    let sum = 0
+    for (const item in salaries) {
+        if (typeof salaries[item] === 'number' && isFinite(salaries[item])) {
+            sum += salaries[item]
+        } else if (salaries == null) {
+            sum = 0
+        }
+    }
+    return sum
 }
