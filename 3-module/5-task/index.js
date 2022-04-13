4321224b08ef4369a897444f3802ac7af5eea960
LizavettaZ <ehkizaveta13@gmail.com>
2022-04-13 10:43:10 +0300

    module 3 tasks

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,2 +1,7 @@
 function getMinMax(str) {
+    const arr = str.replace(/,/g,' ')
+        .split(/\s{1,}/g)
+        .map(item => Number(item))
+        .filter(item => !isNaN(item))
+    return {max: Math.max(...arr) , min:  Math.min(...arr)}
 }
