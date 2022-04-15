69c8d6999ad8395fc4d3640af5513fbbfe7a8537
LizavettaZ <ehkizaveta13@gmail.com>
2022-04-15 11:09:48 +0300

    module 4 tasks

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,2 +1,9 @@
 function makeFriendsList(friends) {
+    const friendsList = document.createElement('ul')
+    friends.map(friend => {
+        const point = document.createElement('li')
+        point.innerHTML = `${friend.firstName} ${friend.lastName}`
+        friendsList.append(point)
+    })
+    return friendsList
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,2 +1,15 @@
 function highlight(table) {
+    const tBody = table.querySelector('tbody')
+
+    for (let row of tBody.rows) {
+        +row.cells[1].innerHTML < 18 ? row.style.textDecoration = 'line-through' : null
+        row.cells[2].innerHTML === 'm' ? row.classList.add('male') :  row.classList.add('female')
+        if (row.cells[3].dataset.available === 'true'){
+            row.classList.add('available')
+        } else if (row.cells[3].dataset.available === 'false'){
+            row.classList.add('unavailable')
+        } else {
+            row.setAttribute('hidden', 'true')
+        }
+    }
 }
