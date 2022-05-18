278b33227e2a4547251daaadab048524dc842e0c
LizavettaZ <ehkizaveta13@gmail.com>
2022-05-18 16:51:31 +0300

    module 8 task 3

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -1,34 +1,62 @@
 export default class Cart {
-  cartItems = []; // [product: {...}, count: N]
+    cartItems = [] // [product: {...}, count: N]
 
-  constructor(cartIcon) {
-    this.cartIcon = cartIcon;
-  }
+    constructor(cartIcon) {
+        this.cartIcon = cartIcon
+    }
 
-  //Добавление товара в массив
-  addProduct(product) {
-  }
+    //Добавление товара в массив
+    addProduct(product) {
+        if (!product) return
 
-  //Изменение количества товара
-  updateProductCount(productId, amount) {
-  }
+        if (this.cartItems.every(cartItem => cartItem.product.name !== product.name)) {
+            this.cartItems.push({ product, count: 1 })
+            this.onProductUpdate({ product, count: 1 })
+        } else {
+            this.cartItems.forEach(cartItem => {
+                if (cartItem.product.name === product.name) {
+                    cartItem.count++
+                    this.onProductUpdate(cartItem)
+                }
+            })
+        }
+    }
 
-  //Пустой ли массив
-  isEmpty() {
-  }
+    //Изменение количества товара
+    updateProductCount(productId, amount) {
+        this.cartItems.forEach((cartItem, index) => {
+            if (cartItem.product.id === productId) {
+                cartItem.count += amount
 
-  //Общее количество товаров в массиве
-  getTotalCount() {
-  }
+                if (cartItem.count === 0) {
+                    this.cartItems.splice(index, 1)
+                }
+            }
+        })
+    }
 
-  //Общая сумма товаров в массиве
-  getTotalPrice() {
-  }
+    //Пустой ли массив
+    isEmpty() {
+        return this.cartItems.length === 0
+    }
 
-  onProductUpdate(cartItem) {
-    // реализуем в следующей задаче
+    //Общее количество товаров в массиве
+    getTotalCount() {
+        return this.cartItems.reduce((previousValue, cartItem) => {
+            return previousValue + cartItem.count
+        }, 0)
+    }
 
-    this.cartIcon.update(this);
-  }
+    //Общая сумма товаров в массиве
+    getTotalPrice() {
+        return this.cartItems.reduce((previousValue, cartItem) => {
+            return previousValue + cartItem.count * cartItem.product.price
+        }, 0)
+    }
+
+    onProductUpdate(cartItem) {
+      // реализуем в следующей задаче
+        this.cartIcon.update(this)
+    }
 }
 
